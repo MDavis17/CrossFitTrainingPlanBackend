8d8f4e2c6d836ff60a2436b332caf538c195d208
Max Davis <madmax.davis@gmail.com>
2019-05-07 21:24:21 -0700

    added connection to google sheets api. not yet acessing sheet for data yet.

---
+++ src/main/java/com/CFSB/MaxDavis/CrossFitTrainingPlanMobile/GoogleSheetProxy.java
@@ -0,0 +1,128 @@
+package com.CFSB.MaxDavis.CrossFitTrainingPlanMobile;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.SheetsScopes;
+import com.google.api.services.sheets.v4.model.ValueRange;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.util.*;
+
+public class GoogleSheetProxy {
+    private static final String APPLICATION_NAME = "CrossFit Training Plan API";
+    private final String spreadsheetId = "1phA5rOnEct0dZ6Xx6ugX5m6IB_sLMPOgKtblgSsOlpY";
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static final String TOKENS_DIRECTORY_PATH = "tokens";
+
+    /**
+     * Global instance of the scopes required by this quickstart.
+     * If modifying these scopes, delete your previously saved tokens/ folder.
+     */
+    private static final List<String> SCOPES = Collections.singletonList(SheetsScopes.SPREADSHEETS_READONLY);
+    private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
+
+    /**
+     * Creates an authorized Credential object.
+     * @param HTTP_TRANSPORT The network HTTP Transport.
+     * @return An authorized Credential object.
+     * @throws IOException If the credentials.json file cannot be found.
+     */
+    private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
+        // Load client secrets.
+        InputStream in = GoogleSheetProxy.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        if (in == null) {
+            throw new FileNotFoundException("Resource not found: " + CREDENTIALS_FILE_PATH);
+        }
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+
+        // Build flow and trigger user authorization request.
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("offline")
+                .build();
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+    }
+
+
+    public String[] getWOD(Date date) throws IOException, GeneralSecurityException {
+        // [Metcon, Gymnastics, Oly, Power, Running]
+        String wodParts[] = {"None","None","None","None","None"};
+
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        final String range = "Class Data!A3:K";
+        Sheets service = new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+        ValueRange response = service.spreadsheets().values()
+                .get(spreadsheetId, range)
+                .execute();
+        List<List<Object>> values = response.getValues();
+        
+        //will want to  pick the correct row from these values by checking the row date (row[0]) against the date that is passed in
+
+        //getting parts of date
+        Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("America/Los_Angeles"));
+        calendar.setTime(date);
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH)+1; // months start at 0
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+
+
+        //TODO: find correct row to get data from (using passed in date)
+        List<Object> row = values.get(0);
+
+        // i is wodParts index, j is index for the workout types in the sheet
+        for(int i = 0, j = 1; i < wodParts.length; i++,j+=2) {
+            if(row.get(j).toString() != "") {
+                wodParts[i] = row.get(j).toString();
+            }
+        }
+
+//        String metcon = row.get(1).toString();
+//        String gymnastics = row.get(3).toString();
+//        String oly = row.get(5).toString();
+//        String power = row.get(7).toString();
+//        String running = row.get(9).toString();
+
+
+        return wodParts;
+    }
+
+    public void printWODs(Date date) throws IOException, GeneralSecurityException {
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+//        final String spreadsheetId = "1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms";
+        final String range = "Class Data!A3:K";
+        Sheets service = new Sheets.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+        ValueRange response = service.spreadsheets().values()
+                .get(spreadsheetId, range)
+                .execute();
+        List<List<Object>> values = response.getValues();
+        if (values == null || values.isEmpty()) {
+            System.out.println("No data found.");
+        } else {
+            System.out.println("printing all olympic workouts:");
+            for (List row : values) {
+                System.out.printf("date: %s, workout: %s\n",row.get(0),row.get(5));
+            }
+        }
+    }
+
+}
--- src/main/java/com/CFSB/MaxDavis/CrossFitTrainingPlanMobile/WODController.java
+++ src/main/java/com/CFSB/MaxDavis/CrossFitTrainingPlanMobile/WODController.java
@@ -4,11 +4,27 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.Date;
+
 @RestController
 public class WODController {
+    GoogleSheetProxy sheetProxy = new GoogleSheetProxy();
 
-    @RequestMapping("/wod")
-    public WOD wod(@RequestParam(value = "metcon",defaultValue = "None") String metcon) {
-        return new WOD(metcon,"None","None","None","None");
+//    @RequestMapping("/wod")
+//    public WOD wod(@RequestParam(value = "metcon",defaultValue = "None") String metcon,
+//                   @RequestParam(value = "gymnastics",defaultValue = "None") String gymnastics,
+//                   @RequestParam(value = "oly",defaultValue = "None") String oly,
+//                   @RequestParam(value = "power",defaultValue = "None") String power,
+//                   @RequestParam(value = "running",defaultValue = "None") String running) {
+//
+//        return new WOD(metcon,gymnastics,oly,power,running);
+//    }
+
+    @RequestMapping ("/wod")
+    public WOD wod() throws IOException, GeneralSecurityException{
+//        sheetProxy.printWODs(new Date());
+        return new WOD("","","","","");
     }
 }
